4ffd694b0b25f14effaf3813705bc7313f8ae2c6
mosmos21 <dev@mosmos21.net>
2023-04-28 19:38:27 +0900

    solve findEntryByStreet

--- exercises/chapter3/test/Main.purs
+++ exercises/chapter3/test/Main.purs
@@ -1,7 +1,7 @@
 module Test.Main where
 
 import Prelude
-import Test.MySolutions
+import Test.MySolutions (findEntryByStreet)
 import Data.AddressBook (AddressBook, Entry, emptyBook, findEntry, insertEntry)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
@@ -59,7 +59,6 @@ main :: Effect Unit
 main =
   runTest do
     runChapterExamples
-    {-  Move this block comment starting point to enable more tests
     suite "Exercise - findEntryByStreet" do
       test "Lookup existing" do
         Assert.equal (Just john)
@@ -67,6 +66,7 @@ main =
       test "Lookup missing" do
         Assert.equal Nothing
           $ findEntryByStreet "456 Nothing St." book
+{-  Move this block comment starting point to enable more tests
     suite "Exercise - isInBook" do
       test "Check existing" do
         Assert.equal true
--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -1,5 +1,12 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.AddressBook (Address, Entry, AddressBook)
+import Data.Maybe (Maybe)
+import Data.List (filter, head)
 
--- Note to reader: Add your solutions to this file
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = head <<< filter findStreet
+    where
+        findStreet :: Entry -> Boolean
+        findStreet entry = entry.address.street == street

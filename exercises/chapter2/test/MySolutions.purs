f241dd24c995f8d59d76405569350bafe8555830
mosmos21 <dev@mosmos21.net>
2023-04-28 18:41:47 +0900

    solve circleArea

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,7 +1,10 @@
 module Test.MySolutions where
 
 import Prelude
-import Data.Number (sqrt)
+import Data.Number (sqrt, pi)
 
 diagonal :: Number -> Number -> Number
 diagonal n p = sqrt $ n * n + p * p
+
+circleArea :: Number -> Number
+circleArea r = pi * r * r

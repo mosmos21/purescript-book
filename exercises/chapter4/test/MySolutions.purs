e7ccaa89be6e2b9c4e4cd2b778e4d80f4cf58096
mosmos21 <dev@mosmos21.net>
2023-04-29 10:00:46 +0900

    solve countEven

--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -1,6 +1,8 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Array (head, tail, null)
+import Data.Maybe (Maybe(..), fromMaybe)
 
 -- Note to reader: Add your solutions to this file
 isEven :: Int -> Boolean
@@ -9,3 +11,13 @@ isEven x = case x of
     1 -> false
     _ | x < 0 -> isEven (-x)
     _ -> isEven (x - 2)
+
+oneIfJustEven :: Maybe Int -> Int
+oneIfJustEven x = case x of
+    Nothing -> 0
+    Just x -> if isEven x then 1 else 0
+
+countEven :: Array Int -> Int
+countEven xs = if null xs
+    then 0
+    else (oneIfJustEven $ head xs) + (countEven $ fromMaybe [] $ tail xs)

e83b8d7efce6709611ea976cda08f0036119284b
mosmos21 <dev@mosmos21.net>
2023-04-29 09:24:11 +0900

    solve isEven

--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -3,3 +3,9 @@ module Test.MySolutions where
 import Prelude
 
 -- Note to reader: Add your solutions to this file
+isEven :: Int -> Boolean
+isEven x = case x of
+    0 -> true
+    1 -> false
+    _ | x < 0 -> isEven (-x)
+    _ -> isEven (x - 2)
